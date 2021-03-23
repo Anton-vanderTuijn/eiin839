30882a75b8cd8c8ced00ab487a576ff5cfe3fc00
Anton van der Tuijn <anton.van-der-tuijn@etu.unice.fr>
2021-03-23 17:04:48 +0100

    TD5

---
+++ TD5/REST/MathsLibrary/IMathsOperations.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MathsLibrary
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IService1" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IMathsOperations
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "Add?a={a}&b={b}", BodyStyle = WebMessageBodyStyle.Wrapped, ResponseFormat = WebMessageFormat.Json)]
+        int Add(int a, int b);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "Multiply?a={a}&b={b}", BodyStyle = WebMessageBodyStyle.Wrapped, ResponseFormat = WebMessageFormat.Json)]
+        int Multiply(int a, int b);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "Subtract?a={a}&b={b}", BodyStyle = WebMessageBodyStyle.Wrapped, ResponseFormat = WebMessageFormat.Json)]
+        int Subtract(int a, int b);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "Devide?a={a}&b={b}", BodyStyle = WebMessageBodyStyle.Wrapped, ResponseFormat = WebMessageFormat.Json)]
+        double Devide(int a, int b);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: ajoutez vos opérations de service ici
+    }
+
+    // Utilisez un contrat de données comme indiqué dans l'exemple ci-après pour ajouter les types composites aux opérations de service.
+    // Vous pouvez ajouter des fichiers XSD au projet. Une fois le projet généré, vous pouvez utiliser directement les types de données qui y sont définis, avec l'espace de noms "MathsLibrary.ContractType".
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ TD5/SOAP/CalculatorTester/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                ServiceReferenceCalculator.MathsOperationsClient client = new ServiceReferenceCalculator.MathsOperationsClient();
+
+                string method = Console.ReadLine();
+                int a = Int32.Parse(Console.ReadLine());
+                int b = Int32.Parse(Console.ReadLine());
+                switch (method.ToLower())
+                {
+                    case "add":
+                        Console.WriteLine(client.Add(a,b));
+                        break;
+                    case "multiply":
+                        Console.WriteLine(client.Multiply(a, b));
+                        break;
+                    case "subtract":
+                        Console.WriteLine(client.Subtract(a, b));
+                        break;
+                    case "devide":
+                        Console.WriteLine(client.Devide(a, b));
+                        break;
+                    default:
+                        Console.WriteLine("One argument is not valid !");
+                        break;
+                }
+
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("\nMissing argument !");
+            }
+            Console.Read();
+
+        }
+    }
+}
